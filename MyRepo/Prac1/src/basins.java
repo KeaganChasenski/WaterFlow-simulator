63d8ce4c53c70b6ec139a5cf5796b86b23a3f210
KeaganChasenski <k.chasenski@gmail.com>
2020-08-25 20:35:10 +0200

    First commit for the project. Main driver class and basins class created

---
+++ MyRepo/Prac1/src/basins.java
@@ -0,0 +1,14 @@
+/* Keagan Chasenski
+ * CHSKEA001
+ * PracOne - Basins
+ * 24/08/20
+*/
+
+import java.util.* ;
+import java.io.* ; 
+
+public class basins
+
+{
+
+}
---
+++ MyRepo/Prac1/src/main.java
@@ -0,0 +1,20 @@
+/* Keagan Chasenski
+ * CHSKEA001
+ * PracOne - Basins
+ * 24/08/20
+*/
+
+import java.util.* ;
+import java.io.* ;
+
+public class main
+{
+    public static void main(String[] args)
+    {
+        Scanner kIn = new Scanner(System.in) ;
+        Scanner fIn = new Scanner() ;
+
+    }
+
+
+}
